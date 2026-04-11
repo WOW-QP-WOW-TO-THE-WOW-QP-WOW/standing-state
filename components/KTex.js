6c2f5f802b67bd0b4ba89b0dc7ce37b15856eb3a
Leon Powdar <leonsp3@gmail.com>
2026-04-11 02:22:35 -0400

    Refactor KTex component to remove inline styles
    Remove inline style for span display property.

--- components/KTex.js
+++ components/KTex.js
@@ -5,16 +5,27 @@
  */
 import katex from 'katex'
 
+
+
 export default function KTex({ math, display = false, className = '' }) {
   const html = katex.renderToString(math, {
     throwOnError: false,
     displayMode: display,
   })
+
+  if (display) {
+    return (
+      <div
+        className={className}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    )
+  }
+
   return (
     <span
       className={className}
       dangerouslySetInnerHTML={{ __html: html }}
-      style={display ? { display: 'block' } : undefined}
     />
   )
 }
