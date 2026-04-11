687bda9815c0b6425604af37345824cd0420aecb
Leon Powdar <leonsp3@gmail.com>
2026-04-11 01:53:39 -0400

    Add KaTeX stylesheet link to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;1,400;1,500&family=Cinzel:wght@400;500;600&display=swap" rel="stylesheet" />
-      </Head>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" />
+ 
+    </Head>
       <body>
         <Main />
         <NextScript />
