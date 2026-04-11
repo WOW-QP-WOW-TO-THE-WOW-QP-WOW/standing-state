0a196390e8cb5a642c0269e20c2a09162a6cb8e5
Leon Powdar <leonsp3@gmail.com>
2026-04-11 00:29:18 -0400

    Create GatesPage component for category entry system

---
+++ pages/system/pages/system/gates/index.js
@@ -0,0 +1,8 @@
+export default function GatesPage() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Gates</h1>
+      <p>Category entry system</p>
+    </div>
+  );
+}
