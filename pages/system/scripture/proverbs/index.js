efb88ed66e1fa5237ace77bdcfd8172e259e1ffb
Leon Powdar <leonsp3@gmail.com>
2026-05-28 14:15:08 -0400

    Add files via upload

--- pages/system/scripture/proverbs/index.js
+++ pages/system/scripture/proverbs/index.js
@@ -25,12 +25,12 @@ export default function ProverbsIndexPage() {
         <div className="wrap navInner">
           <a className="navMark" href="/">Standing State Press</a>
           <ul className="navLinks">
-            <li><a href="/system" style={{ color: 'var(--gold)' }}>System</a></li>
+            <li><a href="/system">System</a></li>
+            <li><a href="/system/scripture">Scripture</a></li>
             <li><a href="/#corpus">Corpus</a></li>
             <li><a href="/system/glossary">Glossary</a></li>
             <li><a href="/system/plates">Plates</a></li>
             <li><a href="/essays">Essays</a></li>
-            <li><a href="/#about">Author</a></li>
           </ul>
         </div>
       </nav>
